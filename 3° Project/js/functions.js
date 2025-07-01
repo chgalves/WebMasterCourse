3884e83d7832935b9cb71c5e6268ab81df7c8388
chgalves <carloshenriquegermanoalves7@gmail.com>
2025-07-01 20:17:54 -0300

    Integrating Google API

--- 3° Project/js/functions.js
+++ 3° Project/js/functions.js
@@ -9,7 +9,7 @@ function initializeMap() {
     };
     map = new google.maps.Map(document.getElementById("map"), mapProperties);
 
-    var content = '<p style="color:black,;font-size:13px;padding:10px 0;">Find me</p>'
+    var content = '<p style="color:black;font-size:13px;padding:10px 0;">Find me</p>';
     addMarker(-6.853435600947959, -35.492322949042226, '', content);
 }
 
@@ -32,4 +32,9 @@ function addMarker(lat, lng, icon, content) {
     marker.addListener('click', function() {
         infoWindow.open(map, marker);
     });
+
+    setTimeout(function() {
+        map.panTo({'lat': 40.71957306353723, 'lng': -74.0207794414495});
+        map.setZoom(8);
+    }, 3000);
 }
