8184e387e00d6aa4fde45bd72270f98940782d52
chgalves <carloshenriquegermanoalves7@gmail.com>
2025-07-01 20:10:04 -0300

    Adding javascript source to integrate Google Maps API

---
+++ 3° Project/js/functions.js
@@ -0,0 +1,35 @@
+var map;
+
+function initializeMap() {
+    var mapProperties = {
+        center: new google.maps.LatLng(-6.853435600947959, -35.492322949042226),
+        scrollWheel: false,
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapProperties);
+
+    var content = '<p style="color:black,;font-size:13px;padding:10px 0;">Find me</p>'
+    addMarker(-6.853435600947959, -35.492322949042226, '', content);
+}
+
+function addMarker(lat, lng, icon, content) {
+    var latLng = { 'lat': lat, 'lng': lng };
+    
+    var marker = new google.maps.marker.AdvancedMarkerElement({
+        position: latLng,
+        map: map,
+        content: content,
+        icon: icon
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: content,
+        maxWidth: 200,
+        pixelOffset: new google.maps.Size(0, 20)
+    });
+
+    marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+    });
+}
